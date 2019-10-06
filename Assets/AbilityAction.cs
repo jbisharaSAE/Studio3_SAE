1f4681b63faadf2effa1fee3397af153a6643294
Anthony <anthonyhsme2@gmail.com>
2019-10-07 03:07:45 +1100

    Created Blast Ability
    Blast ability mechanic added. Player can select multiple tiles, and deploy attacks after selecting all tiles they want. Still a WIP

---
+++ Assets/AbilityAction.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityAction : MonoBehaviour
+{
+    private float step;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    IEnumerator Shoot()
+    {
+        //Get access to abilities script
+        GameObject abilityManager = GameObject.Find("EGO AbilityManager");
+        Abilities abilitiesScript = abilityManager.GetComponent<Abilities>();
+
+        for (int i = 0; i < abilitiesScript.blastProjectileGroup.Count; i++)
+        {
+            //Establish the target for the FIRST projectile (which is the first selectiontile)  - 1
+            Vector3 target = abilitiesScript.selectionTileGroup[i].transform.position;
+
+            //Make first projectile move to target0 position
+            abilitiesScript.blastProjectileGroup[i].transform.position = Vector2.MoveTowards(abilitiesScript.blastProjectileGroup[i].transform.position, target, step);
+
+            //Kill projectile if it touches the target
+            if (abilitiesScript.blastProjectileGroup[i].transform.position == target)
+            {
+                HitTarget();
+            }
+            //Wait time before deploying another projectile
+            yield return new WaitForSeconds(abilitiesScript.blastDeployDelay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set up speed of projectiles
+        step = speed * Time.deltaTime;
+
+        StartCoroutine("Shoot");
+    }
+
+    void HitTarget()
+    {
+        //Destroy the projectile when it lands
+        Destroy(gameObject);
+    }
+}
