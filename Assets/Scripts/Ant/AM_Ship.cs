5482cb399837a8e4e05675ad795f5f8426be885b
Anthony Mitchell <1013307@student.sae.edu.au>
2019-10-11 13:19:13 +1100

    Added 4 SFX files

--- Assets/Scripts/Ant/AM_Ship.cs
+++ Assets/Scripts/Ant/AM_Ship.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class AM_Ship : MonoBehaviour
 {
+    //Variables
+    public int shipHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
