7cd0bf774673603694a4236185d20373cbfe7458
Ray <rzhang48@gmail.com>
2019-10-16 12:05:20 +1100

    Camera change to zoom in

---
+++ Assets/Scripts/RZ_CameraResize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RZ_CameraResize : MonoBehaviour
+{
+    //Assign this Camera in the Inspector
+    public Camera m_OrthographicCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set the size of the viewing volume you'd like the orthographic Camera to pick up (5)
+        m_OrthographicCamera.orthographicSize = Screen.width/19;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
