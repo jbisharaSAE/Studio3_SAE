5341f755d71862eba92389830110dfe85b5edc10
Ray <rzhang48@gmail.com>
2019-10-18 13:16:28 +1100

    Added my own version of the scenes to tests stuff out
    edited the script for the camera to be adjustable from unity instead of the script

--- Assets/Scripts/RZ_CameraResize.cs
+++ Assets/Scripts/RZ_CameraResize.cs
@@ -7,11 +7,13 @@ public class RZ_CameraResize : MonoBehaviour
     //Assign this Camera in the Inspector
     public Camera m_OrthographicCamera;
 
+    public float divider;
+
     // Start is called before the first frame update
     void Start()
     {
         //Set the size of the viewing volume you'd like the orthographic Camera to pick up (5)
-        m_OrthographicCamera.orthographicSize = Screen.width/19;
+        m_OrthographicCamera.orthographicSize = Screen.width/divider;
     }
 
     // Update is called once per frame
