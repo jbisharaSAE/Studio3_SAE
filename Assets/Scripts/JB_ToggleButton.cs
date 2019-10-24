cedd07bbd87998f138de4dd51e170f6addb45d87
James <j.bishara@hotmail.com>
2019-10-24 15:15:28 +1100

    Added in game music / help button

---
+++ Assets/Scripts/JB_ToggleButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JB_ToggleButton : MonoBehaviour
+{
+    private bool toggle = false;
+
+   public void ToggleInfo(GameObject infoboard)
+    {
+        toggle = !toggle;
+
+        infoboard.SetActive(toggle);
+
+    }
+}
